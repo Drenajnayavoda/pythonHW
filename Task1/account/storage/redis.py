c536b55ff2d5e76ff682ce1e5a0f070ef1314ec7
Drenajnayavoda <91183133+Drenajnayavoda@users.noreply.github.com>
2023-05-30 21:58:04 +0300

    Update redis.py

--- Task1/account/storage/redis.py
+++ Task1/account/storage/redis.py
@@ -1,82 +1,72 @@
 from typing import Optional, List
 import redis
+import random
 from account.model import Account
 from account.storage.protocol import AccountsStorageProtocol
 
 
 class AccountsRedisStorage(AccountsStorageProtocol):
     def __init__(self):
-        self.redis_client = redis.Redis()
+        self.db_client = redis.Redis(host='localhost', port=6379, db=0)
 
     def get_all_accounts(self) -> List[Account]:
-        account_keys = self.redis_client.keys("*")
-        accounts = []
-        for key in account_keys:
-            account_data = self.redis_client.hgetall(key)
-            username = key.decode()
-            password = account_data[b"password"].decode()
-            email = account_data[b"email"].decode()
-            account = Account(username=username, password=password, email=email)
-            accounts.append(account)
-        return accounts
-    
+        list_of_ids = []
+        list_of_accounts = []
+
+        for key in self.db_client.scan_iter("account:*"):
+            account_id = int(key.decode().split(":")[1])
+            list_of_ids.append(account_id)
+
+        for account_id in list_of_ids:
+            list_of_accounts.append(self.get_account_by_id(account_id))
+
+        return list_of_accounts
+
     def get_account_by_id(self, account_id: int) -> Optional[Account]:
-        account_keys = self.redis_client.keys("*")
-        for key in account_keys:
-            account_data = self.redis_client.hgetall(key)
-            username = key.decode()
-            password = account_data[b"password"].decode()
-            email = account_data[b"email"].decode()
-            account = Account(username=username, password=password, email=email)
-            if account.id == account_id:
-                return account
+        account_key = f"account:{account_id}"
+        if self.db_client.exists(account_key):
+            account_data = self.db_client.hgetall(account_key)
+            account = Account(
+                account_id,
+                account_data[b'phone_number'].decode(),
+                account_data[b'password'].decode(),
+                account_data[b'status'].decode()
+            )
+            return account
         return None
 
     def mark_account_as_blocked(self, account_id: int):
-        account_keys = self.redis_client.keys("*")
-        for key in account_keys:
-            account_data = self.redis_client.hgetall(key)
-            username = key.decode()
-            password = account_data[b"password"].decode()
-            email = account_data[b"email"].decode()
-            account = Account(username=username, password=password, email=email)
-            if account.id == account_id:
-                account.blocked = True
-                self.update_account(account)
-                return
+        account_key = f"account:{account_id}"
+        if self.db_client.exists(account_key):
+            self.db_client.hset(account_key, "status", "blocked")
 
     def add_account(self) -> int:
-        username = "new_account"
-        password = "password123"
-        email = "new_account@example.com"
-        account = Account(username=username, password=password, email=email)
-        self.create_account(account)
-        return account.id
+        random_id = random.randint(1000, 9999)
+        random_phone = f'8910{random_id}'
+        account_id = random_id
+        account = Account(account_id, random_phone, 'password0000')
+
+        account_key = f"account:{account_id}"
+        account_data = {
+            "id": account.id,
+            "phone_number": account.phone_number,
+            "password": account.password,
+            "status": account.status.value
+        }
+        self.db_client.hmset(account_key, account_data)
+
+        return account_id
 
     def set_account_processing(self, account_id: int) -> Optional[Account]:
-        account_keys = self.redis_client.keys("*")
-        for key in account_keys:
-            account_data = self.redis_client.hgetall(key)
-            username = key.decode()
-            password = account_data[b"password"].decode()
-            email = account_data[b"email"].decode()
-            account = Account(username=username, password=password, email=email)
-            if account.id == account_id:
-                account.status = "processing"
-                self.update_account(account)
-                return account
+        account_key = f"account:{account_id}"
+        if self.db_client.exists(account_key):
+            self.db_client.hset(account_key, "status", "processing")
+            return self.get_account_by_id(account_id)
         return None
 
     def set_account_pending(self, account_id: int) -> Optional[Account]:
-        account_keys = self.redis_client.keys("*")
-        for key in account_keys:
-            account_data = self.redis_client.hgetall(key)
-            username = key.decode()
-            password = account_data[b"password"].decode()
-            email = account_data[b"email"].decode()
-            account = Account(username=username, password=password, email=email)
-            if account.id == account_id:
-                account.status = "pending"
-                self.update_account(account)
-                return account
+        account_key = f"account:{account_id}"
+        if self.db_client.exists(account_key):
+            self.db_client.hset(account_key, "status", "pending")
+            return self.get_account_by_id(account_id)
         return None
